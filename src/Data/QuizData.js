4d8f32baf5b98659d2df1d346307fb3f2b500d39
Gitanjan123 <122338703+Gitanjan123@users.noreply.github.com>
2023-04-02 00:42:26 +0530

    Commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "Who is the 1st boy in college?",
+        options:[ "Gourav", "Abhick", "Arghya",],
+        answer: 1
+    },
+    {
+        question: "Who is the best player in cricket",
+        options:["Ms Dhoni", "Rohit Sharma", "Virat Kohli", "Hardik Pandya"],
+        answer: 2
+    },
+    {
+        question: "Who won the world cup",
+        options: ["Brazil", "Portugal", "Spain", "Argentina"],
+        answer: 4
+    },
+    {
+        question: "Which is the best food",
+        options: ["Egg Roll", "Chowmein", "Chilli Chicken", "Mughlai"],
+        answer:3
+    },
+];
